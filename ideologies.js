11a94071f7fefd2e3d476ea38ccaf8b4c4a13d0d
Marcin Kaletka <marcin.kaletka@gmail.com>
2020-06-26 23:13:34 +0200

    trimmings

--- ideologies.js
+++ ideologies.js
@@ -2,9 +2,9 @@ ideologies = [
     {
         "name": "Communism",
         "stats": {
-            "econ": 100,
+            "econ": 1,
             "dipl": 1,
-            "govt": 1,
+            "govt": 100,
             "scty": 1
         }
     }, {
